3b20ce8168ede5f6c8dd9e17bf29dac5e1de99ea
Sadjad Fallah <m.sadjad.fallah@gmail.com>
2015-09-12 06:23:37 +0430

    Draw On Offer Profile

--- app/src/main/java/ir/appengine/adraaf/OffersActivity.java
+++ app/src/main/java/ir/appengine/adraaf/OffersActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,6 +28,8 @@ import utils.Session;
 public class OffersActivity extends AppCompatActivity {
     static public int current_id;
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +42,15 @@ public class OffersActivity extends AppCompatActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        toolbar = (Toolbar)findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(
+                R.id.fragment_navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout),
+                toolbar);
     }
 
     public static class PlaceholderFragment extends Fragment {
